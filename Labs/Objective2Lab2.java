a2be210a892c7edf045a97504a7bf6379e4216ff
Nathan Smith <nathansmithpiano@gmail.com>
2022-01-15 18:41:34 -0600

    Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Nathan Smith";
+    int age = 33;
+    String state = "Colorado";
+
+    System.out.println(
+      "Name: " + name + " " +
+      "Age: " + age + " " +
+      "State: " + state);
+  }
+}
