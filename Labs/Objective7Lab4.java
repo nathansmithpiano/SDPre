cd4e3df6433588cf89c4801fe62bd572b1dbc7dd
Nathan Smith <nathansmithpiano@gmail.com>
2022-01-15 20:39:44 -0600

    Objective 7 Lab 4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main (String[] args) {
+    int sum = 0;
+    int counter = 1;
+
+    while (counter <= 20) {
+      sum += counter;
+      counter++;
+    }
+
+    System.out.println(sum);
+
+  }
+}
